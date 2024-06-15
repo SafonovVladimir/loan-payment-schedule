9da542426e24b0996a977862474abef9a5129a17
SafonovVladimir <vladimir_safonov@ukr.net>
2024-06-15 15:56:02 +0300

    add endpoint to change payments with id

--- loan_payment_schedule/payments/urls.py
+++ loan_payment_schedule/payments/urls.py
@@ -1,10 +1,18 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import LoanViewSet, PaymentScheduleViewSet, ClientViewSet
+from .views import LoanViewSet, PaymentScheduleViewSet, ClientViewSet, modify_payment_principal
 
 router = DefaultRouter()
 router.register(r"clients", ClientViewSet)
 router.register(r"loans", LoanViewSet)
 router.register(r"schedules", PaymentScheduleViewSet)
 
-urlpatterns = router.urls
+custom_urlpatterns = [
+    path('payments/<int:payment_id>/modify-principal/', modify_payment_principal, name='modify_payment_principal'),
+]
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("", include(custom_urlpatterns)),
+]
